4e526697ca591937f38dae635f29203809eb3442
Great Attractor <great_attractor@Great.Attractor>
2026-02-22 09:07:50 -0300

    Codigo Python corrigido

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-git add app.py
-git commit -m "Nova interface de monitoramento global"
-git push origin master
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>FLYING FORTRESS SECURITY</h1><p>Status: SISTEMA OPERACIONAL - AGUARDANDO COMANDO</p>"
+
+@app.route('/registrar', methods=['POST'])
+def registrar_global():
+    try:
+        dados = request.json
+        return jsonify({"status": "SUCESSO", "msg": "Piloto Autenticado"}), 200
+    except Exception as e:
+        return jsonify({"status": "ERRO", "msg": str(e)}), 400
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
